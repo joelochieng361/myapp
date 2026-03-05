caef1e44dd7588eb68add4bc5e10dd1e6ceb2da9
joelochieng361 <joelochieng361@gmail.com>
2026-03-05 11:33:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Firstcomponent from './components/Firstcomponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to learning react.js</h1>
+        <h2>React is fun</h2>
+
+        {/* Below we show the firstcomponent */}
+        <Firstcomponent />
       </header>
     </div>
   );
--- src/components/Firstcomponent.jsx
+++ src/components/Firstcomponent.jsx
@@ -1,1 +1,16 @@
 //// Create two other components i.e Contactus and Help components and style them with your own content.
+const Firstcomponent = () =>{
+    return(
+        <div className="Firstcomponent">
+            <h1>Welcome to my first component</h1>
+            <h3>about us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a doloremque voluptatibus quae autem tempora consequuntur quibusdam voluptatem similique, perferendis iure harum, aspernatur qui soluta beatae culpa omnis officiis at.</p>
+
+            <h3>Mission</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum corporis suscipit dolores minima sunt architecto repellat accusamus, enim eveniet possimus similique veniam libero fugiat fugit ad natus optio. Quos, debitis!</p>
+        </div>
+    )
+}
+
+//Export your components
+export default Firstcomponent;
