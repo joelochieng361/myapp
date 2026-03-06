a8671a30d7bf8b10e3b8611a1e8c5f98d9b80da1
joelochieng361 <joelochieng361@gmail.com>
2026-03-06 06:20:37 +0000

    ipdate

---
+++ src/components/Sixthcoponent.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const Sixthcomponent = () => {
+    const [weight, setWeight] = useState(50)
+    return(
+        <div className="Sixthcomponet">
+            <h1>Welcome to the Sixth component</h1>
+
+            <h2>Current Weight is: {weight}kg</h2>
+            <button onClick={ () => setWeight(55)}>Update Weight</button>
+        </div>
+    )
+}
