d4a536bb7345222d02e3d713a95a027704af5977
joelochieng361 <joelochieng361@gmail.com>
2026-03-09 08:24:30 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 import Firstcomponent from './components/Firstcomponent';
+import Secondcomponent from './components/Secondcomponent';
+import Contactuscomponent from './components/Contactuscomponent';
+import Helpcomponent from './components/Helpcomponent';
 import Thirdcomponent from './components/Thirdcomponent';
-import Forthcomponent from './components/Forthcomponent';
+import Fourthcomponent from './components/Fourthcomponent';
 import Fifthcomponent from './components/Fifthcomponent';
-import Sixthcomponent from './components/Sixthcoponent';
+import Sixthcomponent from './components/Sixthcomponent';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Notfound from './components/Notfound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to learning react.js</h1>
-        <h2>React is fun</h2>
-        </header>
+   <Router>
+     <div className="App">
 
-        {/* Below we show the firstcomponent */}
-        <Firstcomponent />
-        <Thirdcomponent />
-        <Forthcomponent />
-        <Fifthcomponent />
-        <Sixthcomponent />
+      <nav>
+        <Link to="/first" className='navlinks'>First</Link>
+        <Link to="/second" className='navlinks'>Second</Link>
+        <Link to="/third" className='navlinks'>Third</Link>
+        <Link to="/fourth" className='navlinks'>Fourth</Link>
+        <Link to="/fifth" className='navlinks'>Fifth</Link>
+        <Link to="/sixth" className='navlinks'>Sixth</Link>
+        <Link to="/help" className='navlinks'>Help</Link>
+        <Link to="/contactus" className='navlinks'>Contactus</Link>
+        
+      </nav>
+
+      <header className="App-header">
+        <h1>Welcome to learning reactjs</h1>
+        <h2>React is fun</h2>
+
+
+      </header>
+      {/* Below will render/display/show our component */}
+      <Routes>
+        <Route path="/first" element={<Firstcomponent />} />
+        <Route path="/second" element={<Secondcomponent />} />
+        <Route path="/third" element={<Thirdcomponent />} />
+        <Route path="/fourth" element={<Fourthcomponent />} />
+        <Route path="/fifth" element={<Fifthcomponent />} />
+        <Route path="/sixth" element={<Sixthcomponent />} />
+        <Route path="/help" element={<Helpcomponent />} />
+        <Route path="/contactus" element={<Contactuscomponent />} />
+        <Route path="*" element={<Notfound />} />
+        
+      </Routes>
+      
     </div>
+   </Router>
   );
 }
 
 export default App;
+
+// jsx
+
+// npm start => node package module
+// npx => node package execute
+
+// App.js is our root component
